479625b1bcd6a179fabe8bd1849e513da985cddd
Adarsh Tripathi <tiwarig090909@gmail.com>
2025-08-29 14:01:33 +0530

    Add files via upload

---
+++ adarsh.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	printf("enter first number");
+	"num one"=int(input())
+	printf("enter second number");
+	num two=int(input())
+	res=num one +num two
+	printf("\n Addition result=",res);
+	printf("substraction result=",res);
+	res=num one*num two
+	printf("multiplication result=",res);
+	res=num one/num two
+	printf("division result=",res);=res	
+}
+
+
+	
